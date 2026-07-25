5aef61d12075a501913dadabb9d18789fb7e8feb
md-4n4s <m.d.4n4s@gmail.com>
2026-07-25 14:39:16 +0500

    Found all images' urls

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ response = requests.get(url, headers=headers, timeout=10)
 
 response.raise_for_status()
 
-soup = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(response.text, "html.parser")
 
 links = soup.find_all("a")
 
@@ -22,4 +22,17 @@ pages = set()
 
 for link in links:
     pages.add(link["href"])
+
+img_urls = set()
+
+for page in pages:
+    response = requests.get(urljoin(url,page), headers=headers, timeout=10)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    images = soup.find_all("img")
+
+    for image in images:
+        img_urls.add(image["src"])
 
